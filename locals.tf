2f79a2938d944e08f86ce69458daee83cedab220
krishna <venkatakrishnaredd450@gmail.com>
2026-05-18 20:40:43 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -14,7 +14,7 @@ locals {
     final_ig_tags=merge(
         local.common_tags,
         {
-            Name="${var.project}"-"${var.env}"
+            Name = "${var.project}"-"${var.env}"
         },
         var.ig_tags
     )
