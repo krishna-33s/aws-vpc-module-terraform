73b0171956f4041634d2ebd11e8cddb27f0fc487
krishna <venkatakrishnaredd450@gmail.com>
2026-05-18 20:43:00 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -7,14 +7,14 @@ locals {
     final_vpc_tags = merge(
         local.common_tags,
         {
-            Name = "${var.project}"-"${var.env}"
+            Name = "${var.project}-${var.env}"
         },
         var.vpc_tags
     )
     final_ig_tags=merge(
         local.common_tags,
         {
-            Name = "${var.project}"-"${var.env}"
+            Name = "${var.project}-${var.env}"
         },
         var.ig_tags
     )
