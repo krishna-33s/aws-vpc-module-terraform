5fe215a743db8b54bdfe359be1383d60161d06fe
krishna <venkatakrishnaredd450@gmail.com>
2026-05-19 17:11:21 +0530

    actions

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.cidr_blocks[count.index]
   availability_zone = local.zone_names[count.index]
+  map_public_ip_on_launch = true
 
   tags =merge(
     local.common_tags,
@@ -28,5 +29,81 @@ resource "aws_subnet" "main" {
       Name = "${var.project}-${var.env}-public-${local.zone_names[count.index]}"
     },
     var.public_subnet_tags
+  )
+}
+
+#private subnet
+resource "aws_subnet" "main" {
+  count = length(var.cidr_blocks)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.cidr_blocks[count.index]
+  availability_zone = local.zone_names[count.index]
+  map_private_ip_on_launch = true
+
+  tags =merge(
+    local.common_tags,
+    {
+      #roboshop-dev-private-us-east-1a
+      Name = "${var.project}-${var.env}-private-${local.zone_names[count.index]}"
+    },
+    var.private_subnet_tags
+  )
+}
+
+#database subnet
+resource "aws_subnet" "main" {
+  count = length(var.cidr_blocks)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.cidr_blocks[count.index]
+  availability_zone = local.zone_names[count.index]
+  map_database_ip_on_launch = true
+
+  tags =merge(
+    local.common_tags,
+    {
+      #roboshop-dev-database-us-east-1a
+      Name = "${var.project}-${var.env}-database-${local.zone_names[count.index]}"
+    },
+    var.database_subnet_tags
+  )
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      #roboshop-dev-public
+      Name = "${var.project}-${var.env}-public"
+    },
+    var.public_route_table_tags
+  )
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      #roboshop-dev-private
+      Name = "${var.project}-${var.env}-private"
+    },
+    var.private_route_table_tags
+  )
+}
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      #roboshop-dev-database
+      Name = "${var.project}-${var.env}-database"
+    },
+    var.database_route_table_tags
   )
 }
