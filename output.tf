2f0ebc5bd9bc9cbb37f4c900106f2bf9e96d983b
krishna <venkatakrishnaredd450@gmail.com>
2026-05-23 16:16:31 +0530

    actions

--- output.tf
+++ output.tf
@@ -1,3 +1,19 @@
 output "availability_zones"{
     value = data.aws_availability_zones.available
 }
+
+output "vpc_id"{
+    value = aws_vpc.main.id
+}
+
+output "pubic_subnet"{
+    value = aws_vpc.public[*].id
+}
+
+output "private_subnet"{
+    value = aws_vpc.private[*].id
+}
+
+output "database_subnet"{
+    value = aws_vpc.database[*].id
+}
