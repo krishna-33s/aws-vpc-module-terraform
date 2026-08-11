32c8cf33f6d9e0e2f637c7854b0b0f8b7cbfcce6
krishna <venkatakrishnaredd450@gmail.com>
2026-08-11 15:12:25 +0530

    terraform

--- main.tf
+++ main.tf
@@ -174,6 +174,7 @@ resource "aws_route_table_association" "database" {
   route_table_id = aws_route_table.database.id
 }
 
+# this is for creating database subnet group for RDS
 resource "aws_db_subnet_group" "roboshop" {
   name       = "${var.project}-${var.env}"
   subnet_ids = [aws_subnet.database[0].id, aws_subnet.database[1].id]
